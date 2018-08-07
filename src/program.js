710952ec92c7af462caf44a4374f0253a1f393ed
smakhl <smakhl>
2018-08-07 06:16:55 +0500

    init commit

---
+++ src/program.js
@@ -0,0 +1,8 @@
+const { devices, maxPower, rates } = require("../data/input.json")
+
+class Schedule {
+    constructor({ devices, maxPower, rates }) {
+    }
+}
+
+const schedule = new Schedule({ devices, maxPower, rates })
