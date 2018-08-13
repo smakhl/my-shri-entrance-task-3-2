2f1ccf0a701c3fdb8dfa2412a23fba65b44d3bef
Sergey Makhlonov <SMakhlonov@slb.com>
2018-08-13 15:59:03 +0500

    continue work on algorithm

--- src/program.js
+++ src/program.js
@@ -1,5 +1,7 @@
 const { devices, maxPower, rates } = require("../data/input.json")
 
+const HourlyRates = GetHourlyRates(rates);
+
 class Schedule {
     constructor({ devices, maxPower, rates }) {
         this.schedule = [];
@@ -8,8 +10,13 @@ class Schedule {
         devices.forEach(device => {
             device.possibleStartTime = [];
 
-            for (let index = 0; index <= 24 - device.duration; index++) {
-                device.possibleStartTime.push(index)
+            for (let hour = 0; hour <= 24 - device.duration; hour++) {
+                if (IsTimeUnsuitableForDevice(hour, device)) {
+                    continue;
+                }
+
+                const price = GetDeviceRunPrice(HourlyRates, device, hour);
+                device.possibleStartTime.push({ starttime: hour, price })
             }
         });
 
@@ -18,7 +25,43 @@ class Schedule {
 
 const schedule = new Schedule({ devices, maxPower, rates })
 
-const hourlyRates = {};
-for (let index = 0; index < 24; index++) {
+function IsTimeUnsuitableForDevice(time, device) {
+    if (device.mode == "day" && (time < 7 || time >= 21)) {
+        return true;
+    }
 
+    if (device.mode == "night" && time >= 7 && time < 21) {
+        return true;
+    }
+
+    return false;
+}
+
+function GetHourlyRates(rates) {
+    const hourlyRates = {};
+    rates.forEach(rate => {
+        if (rate.from > rate.to) {
+            for (let hour = rate.from; hour < 24; hour++) {
+                hourlyRates[hour] = rate.value;
+            }
+
+            for (let hour = 0; hour < rate.to; hour++) {
+                hourlyRates[hour] = rate.value;
+            }
+        }
+        else {
+            for (let hour = rate.from; hour < rate.to; hour++) {
+                hourlyRates[hour] = rate.value;
+            }
+        }
+    });
+    return hourlyRates;
+}
+
+function GetDeviceRunPrice(hourlyRates, device, startTime) {
+    let price = 0;
+    for (let hour = startTime; hour < device.duration + startTime; hour++) {
+        price += (device.power / 1000) * hourlyRates[hour];
+    }
+    return price;
 }
