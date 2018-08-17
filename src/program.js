34cf8440a25dfe916e183904a0094901c1b989de
smakhl <smakhl>
2018-08-17 06:50:59 +0500

    continue work on algorithm

--- src/program.js
+++ src/program.js
@@ -6,21 +6,28 @@ class Schedule {
         this.schedule = {};
         this.consumedEnergy = { value: 0, devices: {} };
         this._hourlyRates = [];
-
-
-        rates.forEach(r => this.addRate(r));
-
-        devices.forEach(device => {
-            device.possibleStartTime = [];
-
-            for (let hour = 0; hour <= 24 - device.duration; hour++) {
-                if (this._isTimeOkForDeviceMode(hour, device.mode)) {
-                    const price = this.getDeviceRunPrice(this._hourlyRates, device, hour);
-                    device.possibleStartTime.push({ startTime: hour, price });
-                }
+        this._devices = [];
+        this.maxPower = maxPower;
+        this.modes = {
+            day: {
+                from: 7,
+                to: 20
+            },
+            night: {
+                from: 21,
+                to: 6
             }
+        }
+        rates.forEach(rate => this.addRate(rate));
+        devices.forEach(device => this.addDevice(device));
 
-            device.possibleStartTime.sort((a, b) => {
+        this._makeSchedule();
+    }
+
+    _makeSchedule() {
+        this._devices.sort((a, b) => a.possibleStartTimes.length - b.possibleStartTimes.length);
+        this._devices.forEach(device => {
+            device.possibleStartTimes.sort((a, b) => {
                 const byPrice = a.price - b.price;
                 const byTime = a.startTime - b.startTime;
                 if (byPrice == 0) {
@@ -28,53 +35,55 @@ class Schedule {
                 }
                 return byPrice;
             });
-        });
 
-        devices.sort((a, b) => a.possibleStartTime.length - b.possibleStartTime.length);
+            // const startTime = device.possibleStartTimes[0].startTime;
 
-        devices.forEach(device => {
-            const startTime = device.possibleStartTime[0].startTime;
+            // for (let hour = startTime; hour < device.duration + startTime; hour++) {
+            //     if (hour in this.schedule) {
+            //         this.schedule[hour].push(device.id);
+            //     } else {
+            //         this.schedule[hour] = [device.id];
+            //     }
+            // }
 
-            for (let hour = startTime; hour < device.duration + startTime; hour++) {
-                if (hour in this.schedule) {
-                    this.schedule[hour].push(device.id);
-                } else {
-                    this.schedule[hour] = [device.id];
-                }
-            }
+            device.possibleStartTimes.forEach(startTime => {
+
+            });
         });
     }
 
-    makeSchedule(devices, maxPower, hourlyRates) {
-
-    }
-
-    addRate({ from, to, value }) {
-        if (!arguments[0].hasOwnProperty("from") || !arguments[0].hasOwnProperty("to") || !arguments[0].hasOwnProperty("value")) {
+    addRate(rate) {
+        if (!rate.from || !rate.to || !rate.value) {
             throw "Mandatory parameter is missing";
         }
 
-        if (from < 0 || to > 23)
+        if (rate.from < 0 || rate.to > 23)
             throw "Illegal time";
 
-        if (from > to) {
-            for (let hour = from; hour < 24; hour++) {
-                this._hourlyRates[hour] = value;
-            }
-
-            for (let hour = 0; hour < to; hour++) {
-                this._hourlyRates[hour] = value;
-            }
-        }
-        else {
-            for (let hour = from; hour < to; hour++) {
-                this._hourlyRates[hour] = value;
-            }
+        for (let hour = rate.from; hour !== rate.to; hour == 23 ? hour = 0 : hour++) {
+            this._hourlyRates[hour] = rate.value;
         }
     }
 
-    addDevice() {
+    addDevice(device) {
+        if (this._devices.findIndex(d => d === device) !== -1) {
+            throw "The device is already added";
+        }
 
+        device.possibleStartTimes = [];
+
+        for (let hour = 0; hour <= 24 - device.duration; hour++) {
+            if (this._isTimeOkForDeviceMode(hour, device.mode)) {
+                const price = this._getDeviceRunPrice(device, hour);
+                device.possibleStartTimes.push({ startTime: hour, price });
+            }
+        }
+
+        if (device.possibleStartTimes.length == 0){
+            throw "The device could not be added to the schedule";
+        }
+        
+        this._devices.push(device);
     }
 
     _isTimeOkForDeviceMode(time, mode) {
@@ -89,31 +98,10 @@ class Schedule {
         return true;
     }
 
-    getHourlyRates(rates) {
-        const hourlyRates = {};
-        rates.forEach(rate => {
-            if (rate.from > rate.to) {
-                for (let hour = rate.from; hour < 24; hour++) {
-                    hourlyRates[hour] = rate.value;
-                }
-
-                for (let hour = 0; hour < rate.to; hour++) {
-                    hourlyRates[hour] = rate.value;
-                }
-            }
-            else {
-                for (let hour = rate.from; hour < rate.to; hour++) {
-                    hourlyRates[hour] = rate.value;
-                }
-            }
-        });
-        return hourlyRates;
-    }
-
-    getDeviceRunPrice(hourlyRates, device, startTime) {
+    _getDeviceRunPrice(device, startTime) {
         let price = 0;
         for (let hour = startTime; hour < device.duration + startTime; hour++) {
-            price += (device.power / 1000) * hourlyRates[hour];
+            price += (device.power / 1000) * this._hourlyRates[hour];
         }
         return price;
     }
