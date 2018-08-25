eaa1bea4a8b7ec98f2c7ca7d341a359de37bb813
smakhl <smakhl>
2018-08-25 20:23:37 +0500

    add more tests

--- src/program.test.js
+++ src/program.test.js
@@ -4,6 +4,8 @@ const { devices, maxPower, rates } = require("../data/input.json")
 const schedule = new Schedule({ devices, maxPower, rates })
 const expectedConsumedEnergy = require("../data/output.json").consumedEnergy
 
+const dummyDevice = { id: "123", power: 300, duration: 2, name: "Dummy" };
+
 test("consumed energy value is correct", () => {
     expect(schedule.consumedEnergy.value).toBe(expectedConsumedEnergy.value);
 });
@@ -30,7 +32,7 @@ test("schedule is created for all 24 h", () => {
 
 test("add new device to existing schedule", () => {
     const setDevice = jest.fn(() =>
-        schedule.addDevice({ id: "id", power: 300, duration: 2, name: "test device" })
+        schedule.addDevice(dummyDevice)
     );
     setDevice();
     expect(setDevice).toReturn();
@@ -40,6 +42,22 @@ test("new device to be in schedule", () => {
     expect(Object.keys(schedule.consumedEnergy.devices).length).toBe(6);
 });
 
-test("consumed energy to grow", () => {
+test("consumed energy to grow because of new device", () => {
     expect(schedule.consumedEnergy.value > expectedConsumedEnergy.value).toBe(true);
+});
+
+test("remove new device from schedule", () => {
+    const removeDevice = jest.fn(() =>
+        schedule.removeDevice("123")
+    );
+    removeDevice();
+    expect(removeDevice).toReturn();
+});
+
+test("device count to reduce", () => {
+    expect(Object.keys(schedule.consumedEnergy.devices).length).toBe(5);
+});
+
+test("consumed energy to be back to previous value", () => {
+    expect(schedule.consumedEnergy.value == expectedConsumedEnergy.value).toBe(true);
 });
